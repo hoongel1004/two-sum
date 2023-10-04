c05da9f866705dd3d035d8ce75c08823b2e405d5
hoongel1004 <75716280+hoongel1004@users.noreply.github.com>
2023-10-04 18:32:25 +0900

    main code

---
+++ main.py
@@ -0,0 +1,12 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    nums_map = {}
+    # 키와 값을 바꿔서 딕셔너리로 저장
+    for i, num in enumerate(nums):
+        nums_map[num] = i
+
+    for i, num in enumerate(nums):
+        if target - num in nums_map and i != nums_map[target - num]:
+            return [i, nums_map[target - num]]
+
